5f343a120034112e771e195ca133c464d701fc8b
Yahia Qous <yahiaqous@gmail.com>
2021-07-31 19:26:19 +0300

    Lab01 Done

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,3 +1,6 @@
+from typing import Counter
+
+
 print("""python snakes_cafe.py
 **************************************
 **    Welcome to the Snakes Cafe!   **
@@ -37,15 +40,18 @@ Unicorn Tears
 
 
 order_list=["wings","cookies","spring rolls","salmon","steak","meat tornado","a literal garden","ice cream","cake","pie","coffee","tea","unicorn tears"]
-user_order_list=[]
+
+order_list_counter=[0,0,0,0,0,0,0,0,0,0,0,0,0]
 loop=True
 
 
 while loop:
     user_input = input("""** What would you like to order? **
-***********************************""").lower()
+***********************************
+>""").lower()
 
-
+    counter=0
+    user_order_list=[]
     if user_input=="quit":
         loop=False
         if user_order_list==[]:
@@ -55,8 +61,15 @@ while loop:
 
 
     elif user_input in order_list:
-        user_order_list.append(user_input)
+        order_list_counter[order_list.index(user_input)] += 1 
+        # print (order_list.index(user_input))
         order_details =''
+
+        for i in order_list_counter:
+            if i != 0:
+               user_order_list.append(f'{i} orders of {order_list[counter]}')
+            #    print(user_order_list)
+            counter +=1
 
         for i in user_order_list:
             if order_details =='':
@@ -65,6 +78,7 @@ while loop:
                 order_details = f'{order_details} + {i}'
 
         print(f'{len(user_order_list)} orders contain ({order_details}) have been added to your meal')
+        # print(order_list_counter)
 
 
     else:
